33d231f53def7d9af248417ceb93d07787ff25f8
Quinn Stephens <quinn@quinnstephens.com>
2011-12-29 19:17:19 -0600

    Initial Commit

---
+++ SpaceViking/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SpaceViking
+//
+//  Created by Quinn Stephens on 12/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
